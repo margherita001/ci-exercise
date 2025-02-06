994823c4d74714447de7beafb70e390d3c09ddbf
margherita001 <marghe.altin@gmail.com>
2025-02-06 12:31:30 +0100

    Documentation added

--- example.py
+++ example.py
@@ -1,4 +1,14 @@
 def add(a, b):
+    """
+
+    Args:
+        a (any): number or string
+        b (any): number or string
+
+    Returns:
+        any: adds a and b
+        
+    """    
     return a + b
 
 
